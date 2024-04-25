5c0f34ed1607d9963de39e3b8d6339a508c981d6
Nicolò Taormina <nicktaormina3@gmail.com>
2024-04-25 19:44:24 +0200

    restructured lab03 to use helper function and made a test.py to try a different approach for the lds

--- helper.py
+++ helper.py
@@ -1,24 +1,96 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-# from matplotlib import pyplot as plt
+
+def load_txt(
+	file_path: str,
+	features_type: type = float,
+	return_labels_in_data_matrix: bool = False,
+) -> tuple[np.ndarray, np.ndarray]:
+
+	# Load the array from the text file and convert all but last column to float
+
+	data = np.loadtxt(file_path, delimiter=",", dtype=str)
+
+	if return_labels_in_data_matrix:
+		return data
+
+	# Extract features (all but last column)
+
+	x = data[:, :-1].astype(features_type)
+
+	# Extract labels (last column)
+
+	y = data[:, -1]
+
+	return x, y
 
 
-def load_file(file_path: str) -> tuple[np.ndarray, np.ndarray]:
-    # Load the array from the text file
-    data = np.loadtxt(file_path, delimiter=",", dtype=str)
+def plot_matrix(
+	matrix: np.ndarray,
+	labels: np.ndarray,
+	*,
+	title: str,
+	xlabel: str,
+	ylabel: str,
+	grid: bool = True,
+	invert_yaxis: bool = False,
+) -> None:
 
-    # Extract features (first 4 columns)
-    x = data[:, :-1].astype(float)
+	plt.figure()
 
-    # Extract labels (last column)
-    y = data[:, -1]
+	unique_labels = np.unique(labels)
 
-    return x, y
+	colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_labels)))
+
+	for label, color in zip(unique_labels, colors):
+
+		indices = np.where(labels == label)
+		plt.scatter(matrix[indices, 0], matrix[indices, 1], color=color, label=label)
+
+	plt.title(title)
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+
+
+	plt.legend()
+
+	plt.grid(grid)
+
+	if invert_yaxis:
+		plt.gca().invert_yaxis()
+
+	plt.show()
 
 
 def v_col(x):
-    return x.reshape((x.size, 1))
+
+	return x.reshape((x.size, 1))
 
 
 def v_row(x):
-    return x.reshape((1, x.size))
+
+	return x.reshape((1, x.size))
+
+
+def center_matrix(matrix: np.ndarray) -> np.ndarray:
+
+	# compute mean over the rows
+
+	mu = matrix.mean(0)
+
+	print(f"Mean: \n{mu}")
+
+	return matrix - mu
+
+
+def check_solution() -> None:
+
+	raise NotImplementedError
+
+
+def cv_matrix(matrix: np.ndarray) -> np.ndarray:
+
+	centered_matrix: np.ndarray = center_matrix(matrix)
+
+	return np.dot(centered_matrix.T, centered_matrix) / float(matrix.shape[0])
--- lab03.py
+++ lab03.py
@@ -1,59 +1,12 @@
 import numpy as np
 
 
-from matplotlib import pyplot as plt
-
-
-# from Helpers.helper import load_file
-
-
-def check_solution() -> None:
-    raise NotImplementedError
-
-
-def load_file(
-    file_path: str, return_labels_in_data_matrix: bool = False
-) -> tuple[np.ndarray, np.ndarray]:
-
-    # Load the array from the text file and convert all but last column to float
-
-    data = np.loadtxt(file_path, delimiter=",", dtype=str)
-
-    if return_labels_in_data_matrix:
-        return data
-
-    # Extract features (first 4 columns)
-
-    x = data[:, :-1].astype(float)
-
-    # Extract labels (last column)
-
-    y = data[:, -1]
-
-    return x, y
-
-
-def center_matrix(matrix: np.ndarray) -> np.ndarray:
-
-    # compute mean over the rows
-
-    mu = matrix.mean(0)
-
-    print(f"Mean: \n{mu}")
-
-    return matrix - mu
-
-
-def covariance_matrix(matrix: np.ndarray) -> np.ndarray:
-
-    centered_matrix: np.ndarray = center_matrix(matrix)
-
-    return np.dot(centered_matrix.T, centered_matrix) / float(matrix.shape[0])
+from helper import load_txt, center_matrix, cv_matrix, plot_matrix
 
 
 def projection_matrix(matrix: np.ndarray, m: int) -> np.ndarray:
 
-    cm = covariance_matrix(matrix)
+    cm = cv_matrix(matrix)
 
     print(f"Covariance matrix shape: {cm.shape}")
 
@@ -73,68 +26,45 @@ def projection_matrix(matrix: np.ndarray, m: int) -> np.ndarray:
 def between_class_covariance(matrix: np.ndarray, labels: np.ndarray) -> np.ndarray:
 
     unique_labels = np.unique(labels)
+
     for label in unique_labels:
+
         indices = np.where(labels == label)
+
         swc = center_matrix(matrix[indices, :])
-        
+
     return matrix
 
 
-def plot_pca(projection: np.ndarray, labels: np.ndarray) -> None:
-
-    plt.figure()
-
-    unique_labels = np.unique(labels)
-
-    colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_labels)))
-
-    for label, color in zip(unique_labels, colors):
-
-        indices = np.where(labels == label)
-        plt.scatter(
-            projection[indices, 0], projection[indices, 1], color=color, label=label
-        )
-
-    plt.xlabel("Principal Component 1")
-
-    plt.ylabel("Principal Component 2")
-
-    plt.title("PCA Projection of Iris Dataset")
-
-    plt.legend()
-
-    plt.grid(True)
-
-    plt.gca().invert_yaxis()
-
-    plt.show()
-
-
 def pca() -> None:
-    x, y = load_file("datasets/iris.csv")
+
+    x, y = load_txt("datasets/iris.csv")
 
     print(f"matrix shape {x.shape}")
 
     m2 = projection_matrix(x, 2)
 
     # print(f"Projected matrix:\n {m2[:5]}")
+
     # print(f"Projected matrix shape {m2.shape}")
 
-    plot_pca(m2, y)
+    plot_matrix(
+        m2,
+        y,
+        title="PCA Projection of Iris Dataset",
+        xlabel="Principal Component 1",
+        ylabel="Principal Component 2",
+        invert_yaxis=True,
+    )
 
 
 def lda() -> None:
 
-    x, y = load_file("datasets/iris.csv")
-
-    # labels_map: dict[str, int] = {"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2}
-
-    xs = separate_data_by_label(x, y)
-    print(f"shape: {xs[1]}")
+    x, y = load_txt("datasets/iris.csv")
 
 
 def main() -> None:
-    lda()
+    pca()
 
 
 if __name__ == "__main__":
